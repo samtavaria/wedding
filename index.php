ad89d559a5c69de748d62e342e57398d5a26660e
Kayomars Batliwalla <kayomarsb@hotmail.com>
2013-10-02 21:02:41 -0700

    Integrated new login page

--- index.php
+++ index.php
@@ -1,21 +1,113 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-<script type = "text/javascript">
-    function formValidate() {
-        if(document.getElementById('username').value.length == 0 || document.getElementById('password').value.length == 0) {
-            alert("Username/Password not recognized");
-            return false;
+    <style type="text/css">
+        h1 {
+            font-family: forte, cursive, Serif;
         }
-    }
-</script>
+
+        h1 {
+            color: black
+        }
+
+        ;
+        h1 {
+            font-size: 100%
+        }
+
+        ;
+
+
+    </style>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <!--Facebook Like Button OpenGraph Settings Start-->
+
+    <!--Facebook Like Button OpenGraph Settings End-->
+
+    <title>
+        Create Free Wedding Websites | Sites with Indian Designs |khwdfjsn </title>
+
+    <meta name="description"
+          content="Getting Married? Create your Free Wedding Website with Indian Designs! It takes only a minute to create your wedding site! Try Now!"/>
+
+
+    <!-- <link rel="stylesheet" type="text/css" href="http://myshaadi.in/main/css/prettyPhoto.css" /> -->
+    <link rel="stylesheet" type="text/css" href=""/>
+    <!-- <link rel="stylesheet" type="text/css" href="http://myshaadi.in/main/css/slider.css" /> -->
+    <link rel="stylesheet" type="text/css" href=""/>
+
+    <script type="text/javascript">
+        window.$zopim || (function (d, s) {
+            var z = $zopim = function (c) {
+                z._.push(c)
+            }, $ = z.s =
+                    d.createElement(s), e = d.getElementsByTagName(s)[0];
+            z.set = function (o) {
+                z.set.
+                        _.push(o)
+            };
+            z._ = [];
+            z.set._ = [];
+            $.async = !0;
+            $.setAttribute('charset', 'utf-8');
+            $.src = '//cdn.zopim.com/?lCPLq0U4wY637dSDw1qeNapqscL5yLKM';
+            z.t = +new Date;
+            $.
+                    type = 'text/javascript';
+            e.parentNode.insertBefore($, e)
+        })(document, 'script');
+    </script>
+    <!--End of Zopim Live Chat Script-->
+
+    <script type="text/javascript">
+        function formValidate() {
+            if (document.getElementById('username').value.length == 0 || document.getElementById('password').value.length == 0) {
+                alert("Please enter a valid username/password");
+                document.getElementById('username').focus();
+                return false;
+            }
+        }
+    </script>
+
+
 </head>
-<body>
-<form method = "POST" action = "LoginProcess.php" onsubmit="return formValidate();">
-    Username:<br />
-    <input type = "text" id = "username" name = "username" autocomplete="off"><br />
-    Password: <br />
-    <input type = "password" id = "password" name = "password" autocomplete="off"><br />
-    <input type="submit" value = "submit">
-    </form>
+
+<body class="home" background="website_images/background.jpg">
+
+
+<div align='center'>
+    <h1><font size='100px'>PHERE.COM</font>
+
+        <h1>
+
+
+</div>
+<br><br><br>
+
+
+<div style = "float: left; width: 90%">
+<span style="float:left; margin-left:400px">
+  <img src="website_images/website.jpg" style="width:150px; height:150px; border: none;"/>
+  <br/>
+  <p style="font-family: forte, cursive, Serif;">WEBSITE DESIGN</p>
+</span>
+
+
+<span style="float:left; margin-left:250px">
+  <img src="website_images/invitation.jpg" style="width:150px; height:150px; border: none;"/>
+  <br/>
+<p style="font-family: forte, cursive, Serif;">INVITATION DESIGN</p>
+</span>
+</div>
+<div style = "float: left; margin-left: 40%">
+<form method="POST" action="LoginProcess.php" onsubmit="return formValidate();">
+    Username:<br/>
+    <input type="text" id="username" name="username" autocomplete="off"><br/>
+    Password: <br/>
+    <input type="password" id="password" name="password" autocomplete="off"><br/>
+    <input type="submit" value="submit">
+</form>
+</div>
 </body>
+
 </html>
