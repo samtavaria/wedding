b28508cd21aea9a7f9e559d7e0752975647c77e6
Kayomars Batliwalla <kayomarsb@hotmail.com>
2013-10-04 00:22:10 -0700

    Drag and drop with transparent text box workin. Have to do saving of the modified text

--- Invitation.php
+++ Invitation.php
@@ -18,6 +18,7 @@ $backgroundImage = DatabaseConnection::getInvitationBackground($_SESSION['cid'])
 
 
 
+
 <script type = "text/javascript" >
 
 function changeColor() {
@@ -31,7 +32,29 @@ function changeColor() {
 }
 
 </script>
+
+    <script src="jquery-1.9.1.js"></script>
+    <script src="jquery-ui.js"></script>
+    <script type="text/javascript" src="farbtastic.js"></script>
+    <script type="text/javascript" src="farbtastic/farbtastic.js"></script>
+    <link rel="stylesheet" href="farbtastic/farbtastic.css" type="text/css" />
+
+    <script type ="text/javascript" >
+
+
+        $(document).ready(function()
+        {
+            $( "#bride" ).draggable( { cancel: '' } );
+            $( "#groom" ).draggable( { cancel: '' } );
+            $( "#weds" ).draggable( { cancel: '' } );
+            $( "#library_img" ).draggable( { cancel: '' } );
+
+        });
+
+    </script>
+
 </head>
+
 
 <body>
 
@@ -40,16 +63,30 @@ function changeColor() {
 <div  id = "mySource" style = "float:left">
 
 <form method = "POST" action = "process.php" >
-<input id = "bride" name = "bride" class = "myInputBox" width="500px" onmouseover="this.style.width = ((this.value.length + 1) * 8) + 'px';" style = "outline:none" type = "text"  value = "<?php echo $couple[0]." ".$couple[1]." ".$couple[2]; ?>" > <br />WEDS <br />
-<input id = "groom"  class = "myInputBox" name = "groom" width="500px" onmouseover="this.style.width = ((this.value.length + 1) * 8) + 'px';"  type = "text" value = "<?php echo $couple[3]." ".$couple[4]." ".$couple[5]; ?>" > <br />
-<input type = "hidden" id = "myCss" name ="myCss">
+    <div id="draggable" class="ui-widget-content">
+        <br />
+        <br />
+ <input id = "bride" style = "background-color: transparent" name = "bride" class = "myInputBox" width="500px" onmouseover="this.style.width = ((this.value.length + 1) * 8) + 'px';" style = "outline:none" type = "text"  value = "<?php echo $couple[0]." ".$couple[1]." ".$couple[2]; ?>" >
+
+        <br />
+        <div id = "weds" >WEDS </div>
+        <br />
+
+
+ <input id = "groom"  style = "background-color: transparent" class = "myInputBox" name = "groom" width="500px" onmouseover="this.style.width = ((this.value.length + 1) * 8) + 'px';"  type = "text" value = "<?php echo $couple[3]." ".$couple[4]." ".$couple[5]; ?>" > <br />
+</div>
+        <input type = "hidden" id = "myCss" name ="myCss">
 <input type = "submit" value = "Save" >
+    <img src = "invitation_card_backgrounds_preview/1.jpeg" id = "library_img">
+
 </form>
 
 </div>
-
-<div id = "myCanvas" style = "float: right">
+<!--<input type="text" name="text_color" id="text_color" value=""/> -->
+<div id = "myCanvas" style = "float: right; margin-right:30%">
+    <div id="droppable" class="ui-widget-header">
     <img src = "<?php echo $backgroundImage; ?>" id = "card_background" height = "640" width = "720">
+        </div>
 </div>
 
 <style id = "myStyle" type = "text/css">
@@ -58,7 +95,7 @@ function changeColor() {
 
     </style>
 
-<input type = "button" value = "changeColor" onClick = "changeColor()">
-
+<!--input type = "button" value = "changeColor" onClick = "changeColor()"-->
+<div id="text_picker" style = "display: none"></div>
 </body>
 </html>
