2a057277ee88f9503965cb1bbf0d544c2bdcdbeb
Kayomars Batliwalla <kayomarsb@hotmail.com>
2013-11-19 23:13:01 -0800

    Added rajani's changes for css and logout  and images

---
+++ return_homepage.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    
+</head>
+<body>
+    <a href="index.php"><img id="login" src="images/log_off.png"></a>
+    <a href="Menu.php"><img id="home" src="images/home.png"></a>
+</body>
+</html>
+
