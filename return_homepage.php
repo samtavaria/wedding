4e362dec449b53b3b90d32b383f43a6f6aa9d2f0
Kristen Morse <kristenmorsecode@gmail.com>
2013-11-24 03:17:34 -0800

    changed navbar color

--- return_homepage.php
+++ return_homepage.php
@@ -2,7 +2,7 @@
 <head>
 <link rel="stylesheet" href="dist/css/bootstrap.css">
 
-<nav class="navbar navbar-default" role="navigation">
+<nav style="border-color:black;background-color:#580000; color:yellow;"class="navbar navbar-default" role="navigation">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
       <span class="sr-only">Toggle navigation</span>
