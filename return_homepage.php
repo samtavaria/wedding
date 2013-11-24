0a5761d00e4f55d8c5ee66fe64659e9e3c2a7529
Kristen Morse <kristenmorsecode@gmail.com>
2013-11-24 01:40:35 -0800

    added navbar accross all pages. removed login logout buttons.

--- return_homepage.php
+++ return_homepage.php
@@ -1,29 +1,37 @@
 <html>
 <head>
-<style type="text/css">
-#home
-{
-    position:absolute;
-    right:95px;
-    width:80px;
-    height:80px;
-    top:15px;
-}
+<link rel="stylesheet" href="dist/css/bootstrap.css">
 
-#logout
-{
-    position:absolute;
-    right:15px;
-    width:80px;
-    height:80px;
-    top:15px;
-}
-</style>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Phere.com</a>
+  </div>
+
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li><a href="Menu.php">Home</a></li>
+      <li><a href="TemplateSelection.php">Invitation Design</a></li>
+      <li><a href="GuestManagement.php">Guest Management</a></li>
+      <li><a href="personal_website/index.php">Personal Website</a></li>
+      <li><a href="survey.php">Survey</a></li>
+      <li><a href="email_page.php">Email Guests</a></li>
+    </ul>
+
+<ul class="nav navbar-nav navbar-right">
+      <li><a href="index.php">Logout</a></li>
+    </ul>
+
+
+  </div><!-- /.navbar-collapse -->
+</nav>
 
 </head>
-<body>
-    <a href="index.php"><img id="logout" src="images/log_off.png"></a>
-    <a href="Menu.php"><img id="home" src="images/home.png"></a>
-</body>
+
 </html>
 
---
+++ return_homepage_old.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<style type="text/css">
+#home
+{
+    position:absolute;
+    right:95px;
+    width:80px;
+    height:80px;
+    top:15px;
+}
+
+#logout
+{
+    position:absolute;
+    right:15px;
+    width:80px;
+    height:80px;
+    top:15px;
+}
+</style>
+
+</head>
+<body>
+    <a href="index.php"><img id="logout" src="images/log_off.png"></a>
+    <a href="Menu.php"><img id="home" src="images/home.png"></a>
+</body>
+</html>
+
